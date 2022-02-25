a2efccacf6bd1228f97034ce9bafd3408d7e73c3
shruthirai69 <98947166+shruthirai69@users.noreply.github.com>
2022-02-25 14:02:37 +0530

    Add files via upload

---
+++ prg2client.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+int main()
+{
+char fname[50],buffer[1024];
+int s,n,source,file;
+struct sockaddr_in address;
+address.sin_adress=AF_INET;
+address.sin_port=htons(15000);
+address.sin_addr.s_addr=INADDR_ANY;
+printf("Waiting request");
+s=socket(AF_INET,SOCK_STREAM,0);
+bind(s,(struct sockaddr*)&address,sizeof(address));
+listen(s,3);
+source=accept(s,(struct sockaddr*)NULL,NULL);
+recv(source,fname,sizeof(fname),0);
+printf("request for file");
+file=open(fname,O_RDONLY);
+if(file<0)
+{
+printf("Print not found");
+send(source,"File not found",20,0);
+}
+while((n=read(file,buffer,sizeof(buffer)))>0)
+send(source,buffer,n,0);
+printf(" request sent");
+close(file);
+return 0;
+}
