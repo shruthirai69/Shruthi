788832109f36b837256e0123262fd138b8a5e637
shruthirai69 <98947166+shruthirai69@users.noreply.github.com>
2022-02-25 21:18:08 +0530

    Update prgm2.sh

--- shell script/prgm2.sh
+++ shell script/prgm2.sh
@@ -1,3 +1,7 @@
+#2a. Write a shell script that accepts two filenames as arguments, checks if the permissions for 
+these files are identical and if the permissions are identical, output common permissions 
+otherwise output each filename followed by its permissions. 
+
 echo enter first file name
 read f1
 if [ -e $f1 ]
