42bee7e522455b4e0ca7275a274c57b08824217e
shruthirai69 <98947166+shruthirai69@users.noreply.github.com>
2022-02-25 21:30:44 +0530

    Update prgm4.sh

--- shell script/prgm4.sh
+++ shell script/prgm4.sh
@@ -1,3 +1,6 @@
+#4a. Write a shell script that accept one or more file names as argument and convert all of them 
+to uppercase, provided they exists in current directory.
+
 for i in $*
 do 
 if [ -f $i ]
