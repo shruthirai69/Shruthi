e5d07d429c1d614ac485752a69d08158371ebcf5
shruthirai69 <98947166+shruthirai69@users.noreply.github.com>
2022-04-06 16:02:09 +0530

    Add files via upload

---
+++ shell script/9a.sh
@@ -0,0 +1,13 @@
+if [ $# -eq 3 ]
+then
+	if [ -f $1 ]
+	then
+		l=`wc -l $1`
+		head -n $3 $1 | tail +$2
+	else
+		echo "Enter available $i is not a file"
+	fi
+else
+	echo "Enter the required argument"
+	echo "usage file name sarting line ending line"
+fi
